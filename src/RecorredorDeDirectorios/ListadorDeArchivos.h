785bc90cc9972b87cd4f31913dd6d82b58455b79
lauta1608 <lauta1608@gmail.com>
2012-09-01 16:14:18 +0000

    Archivo cabecera para el listado recursivo de archivos dentro de un directorio

---
+++ src/RecorredorDeDirectorios/ListadorDeArchivos.h
@@ -0,0 +1,9 @@
+#ifndef __RECORREDORDEDIRECTORIOS_H__
+#define __RECORREDORDEDIRECTORIOS_H__
+
+#include <iostream>
+#include <list>
+
+void listarArchivos (std::string ruta, std::list<std::string>* lista); 
+
+#endif
