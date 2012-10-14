9b51967d78c20c3f840923b99465ff6a30e7d680
fferreyra38@gmail.com <fferreyra38@gmail.com@ba8bd6c1-2fa0-96db-9c5d-c82f4668fce7>
2012-10-14 12:20:31 +0000

    adelanto de persister, hay que refactorizar la manera en que vuelca a disco

--- trunk/src/codigo_tp/Persister.h
+++ trunk/src/codigo_tp/Persister.h
@@ -1,11 +1,31 @@
 #ifndef __PERSISTER_H_
 #define __PERSISTER_H_
 
+#include <fstream>
+#include <string>
+#include <list>
+#include "trie.h"
+
+struct TregistroArchivo{
+    int col;
+    int fil;
+    int freq;
+};
+
 class Persister {
-	private:
-	
+
 	public:
-	
+        Persister(std::string);
+        ~Persister();
+        void recibirDatos(list<TnodoData*>*,unsigned int);
+
+	private:
+
+        void bajarDatosADisco();
+
+        fstream salida;
+        list<TregistroArchivo*>* contenedor;
+
 };
 
 #endif
