bfc5db6cfa831b497e8fb7ad211e204506e20da2
fferreyra38 <fferreyra38@gmail.com>
2012-10-14 12:20:31 +0000

    adelanto de persister, hay que refactorizar la manera en que vuelca a disco

--- src/codigo_tp/Persister.cpp
+++ src/codigo_tp/Persister.cpp
@@ -1,1 +1,32 @@
 #include "Persister.h"
+
+Persister::Persister(std::string path){
+
+    salida.open(path.c_str(),fstream::out);
+    //inicializo cabecera del archivo
+    salida<<"%%MatrixMarket matrix coordinate real general"<<endl;
+    //se debe dejar un espacio en blanco para luego insertar la cantidad de col, fil e indices
+    //ese debe ser el formato respetado para que la libreria pueda andar
+    salida<<"                         "<<endl;
+
+    contenedor = new list<TregistroArchivo*>;
+
+}
+
+Persister::~Persister(){
+
+    salida.close();
+    //vaciar contenedor
+
+}
+
+void Persister::recibirDatos(list<TnodoData*>* data, unsigned int columna){
+
+    list<TnodoData*>::iterator b = data->begin();
+    list<TnodoData*>::iterator e = data->end();
+    while(b!=e){
+        TnodoData* aux = *b;
+        salida<<columna<<" "<<aux->id<<" "<<aux->ocurrenciasEnElDocActual<<endl;
+        b++;
+    }
+}
