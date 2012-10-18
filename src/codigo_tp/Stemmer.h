917cd9d750086dc4e7e6ea0ba971cbfc92af4119
aeeisnerg <aeeisnerg@gmail.com>
2012-10-18 23:55:38 +0000

    Agregado el algoritmo de Porter. No lo probé, confío en en chabón que escribió el código...

--- src/codigo_tp/Stemmer.h
+++ src/codigo_tp/Stemmer.h
@@ -1,12 +1,45 @@
 #ifndef __STEMMER_H_
 #define __STEMMER_H_
 
+#include <cstring>  /* for memmove */
+#include <cstdio>
+#include <cstdlib>      /* for malloc, free */
+#include <cctype>       /* for isupper, islower, tolower */
+
+#define INC 50           /* size units in which s is increased */
+#define LETTER(ch) (isupper(ch) || islower(ch))
+
+
+
 class Stemmer {
-	public:
-		Stemmer();
-		~Stemmer();
-		
-		std::string stemPalabra(std::string palabra);
+
+private:
+	static char * b;       /* buffer for word to be stemmed */
+	static int k,k0,j;     /* j is a general offset into the string */
+	static char * s;         /* a char * (=string) pointer; passed into b above */
+	static int i_max = INC;  /* maximum offset in s */
+
+
+public:
+	Stemmer();
+	static void stemfile(FILE * f);
+	void increase_s();
+	int stem(char * p, int i, int j);
+	static void step5();
+	static void step4();
+	static void step3();
+	static void step2();
+	static void step1ab();
+	static void step1c();
+	static void r(char * s);
+	static void setto(char * s);
+	static int ends(char * s);
+	static int cvc(int i);
+	static int doublec(int j);
+	static int vowelinstem();
+	static int m();
+	static int cons(int i);
+	~Stemmer();
 };
 
 #endif
