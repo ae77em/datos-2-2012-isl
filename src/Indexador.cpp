eed4047c9500e344a8d339e441c2025f0ab44836
lauta1608 <lauta1608@gmail.com>
2012-11-04 23:20:44 +0000

    Agregado de funcionalidad para las oraciones

--- src/Indexador.cpp
+++ src/Indexador.cpp
@@ -23,43 +23,45 @@ Indexador::~Indexador() {
  * Realiza los procesos necesarios para la creacion del indice
  */
 bool Indexador::crearIndice(std::string nombreRepositorio, int cantTopicos, std::string directorio) {
-
 	std::list<std::string>* archivos = listador->listarArchivos(directorio);
 
 	std::list<std::string>::iterator iterador;
-
 	for (iterador = archivos->begin(); iterador != archivos->end(); iterador++) {
-
 		std::string nombreArchivo = (*iterador);
 
 		if (!parser->parsearArchivo(nombreArchivo)) {
 			std::cerr << "El archivo " << nombreArchivo << " no pudo ser parseado" << std::endl;
 		}else{
+			parser->obtenerContenedorLexico()->aumentarCantidadDeDocParseados();
+			parser->obtenerContenedorOraciones()->aumentarCantidadDeDocParseados();
 
-		parser->obtenerContenedorLexico()->aumentarCantidadDeDocParseados();
+			// vuelvo los datos obtenidos del documento parseado a disco
+			persistidor->persistirDatos(parser->obtenerContenedorLexico()->buscarPalabrasDelDocParseado_INI(), parser->obtenerContenedorLexico()->obtenerCantidadDeDocumentosParseados());
+			persistidor->persistirDatos(parser->obtenerContenedorOraciones()->buscarPalabrasDelDocParseado_INI(), parser->obtenerContenedorOraciones()->obtenerCantidadDeDocumentosParseados());
 
-		// vuelvo los datos obtenidos del documento parseado a disco
-		persistidor->persistirDatos(parser->obtenerContenedorLexico()->buscarPalabrasDelDocParseado_INI(), parser->obtenerContenedorLexico()->obtenerCantidadDeDocumentosParseados());
+			//preparo el trie para una nueva parseada
+			parser->obtenerContenedorLexico()->inicializarFrecuenciasLocales();
+			parser->obtenerContenedorLexico()->vaciarContenedorParcial();
 
-
-		//preparo el trie para una nueva parseada
-		parser->obtenerContenedorLexico()->inicializarFrecuenciasLocales();
-		parser->obtenerContenedorLexico()->vaciarContenedorParcial();
+			parser->obtenerContenedorOraciones()->inicializarFrecuenciasLocales();
+			parser->obtenerContenedorOraciones()->vaciarContenedorParcial();
 		}
 	}
 
 //	ponderador->ponderar(parser->obtenerContenedorLexico(), persistidor);
     parser->persistirLexico();
-
-	std::cout << "IDs: " << parser->obtenerContenedorLexico()->obtenerContadorId()<< std::endl;
+    parser->persistirOraciones();
 
     calculador->calcularLSI(cantTopicos);
-
     persistidor->parserLSI(3);
-
     persistidor->cerrar();
 
-
+    // TODO para las oraciones, no se como se hace
+    /*
+    calculador->calcularLSI(cantTopicos);
+	persistidor->parserLSI(3);
+	persistidor->cerrar();
+    */
     delete archivos;
 	return true;
 
