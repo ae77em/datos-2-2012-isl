17c2317a46852b12c25eff8b6b09257cd4ec5f02
fferreyra38 <fferreyra38@ba8bd6c1-2fa0-96db-9c5d-c82f4668fce7>
2012-11-18 02:02:25 +0000

    limpie viaje clase consulter solo le deje los metodos que se relacionan con el metodo del coseno

--- trunk/src/Consulter/Consulter.cpp
+++ trunk/src/Consulter/Consulter.cpp
@@ -19,33 +19,6 @@ double Consulter::metodoCoseno(std::vector<double>* v1,std::vector<double>* v2){
     return ( numerador / (norma1 * norma2) ) ;
 }
 
-int Consulter::buscarIdTermino(std::string termino,std::vector<int>* offsetLexico,std::fstream* lexico){
-
-    int ini=0;
-    int fin=offsetLexico->size()-1;
-    int medio = fin / 2;
-
-    return buscarIdTerminoRec(termino,offsetLexico,ini,fin,medio,lexico);
-
-}
-
-
-std::vector<int>* Consulter::obtenerOffsetLexico(std::string path){
-
-//levanto offset del lexico
-    std::fstream offsetLexico;
-    std::vector<int>* offset = new std::vector<int>;
-    offsetLexico.open("offsetLexico.txt",std::fstream::in);
-    int nro=0;
-    while(!offsetLexico.eof()){
-        offsetLexico>>nro;
-        offset->push_back(nro);
-    }
-    offsetLexico.close();
-
-    return offset;
-
-}
 
 
 double Consulter::productoInterno(std::vector<double>* v1,std::vector<double>* v2){
@@ -72,37 +45,4 @@ double Consulter::norma(std::vector<double>* v){
 
 }
 
-int Consulter::buscarIdTerminoRec(std::string termino,std::vector<int>* offsetLexico,int ini,int fin,int medio,std::fstream* lexico){
 
-    std::string terminoEnArchivo="";
-    int id=0;
-
-    //condicion de corte
-    if(ini==fin){
-        lexico->seekg(offsetLexico->at(ini));
-        *lexico>>terminoEnArchivo;
-        *lexico>>id;
-        return (terminoEnArchivo.compare(termino)==0)?id:-1;
-    }
-
-    lexico->seekg(offsetLexico->at(medio));
-    *lexico>>terminoEnArchivo;
-    *lexico>>id;
-    if( terminoEnArchivo.compare(termino)==0){
-        return id;
-    }else{
-
-        if(termino.compare(terminoEnArchivo)==-1){
-            fin= medio -1;
-            medio= fin/2;
-            return buscarIdTerminoRec(termino,offsetLexico,ini,fin,medio,lexico);
-
-            }else{
-            ini= medio + 1;
-            medio= (medio + fin)/2;
-            return buscarIdTerminoRec(termino,offsetLexico,ini,fin,medio,lexico);
-
-        }
-    }
-
-}
